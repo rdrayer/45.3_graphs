1d490bf042031c68c308504c190d5452b0101ee3
Roberta Drayer <robertadrayer@gmail.com>
2024-02-17 16:58:42 -0800

    addVertices

--- graph.js
+++ graph.js
@@ -12,14 +12,20 @@ class Graph {
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
   addVertex(vertex) {
-    this.nodes.add(vertex)
+    this.nodes.add(vertex);
   }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.nodes.add(vertex);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
   removeEdge(v1, v2) {}
